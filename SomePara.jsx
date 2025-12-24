b90890cf4d4fa7461d7e8980eb44f85c63dee7bb
AnasLalaMemon <c53.rm3760@gmail.com>
2025-12-24 16:20:04 +0500

    Add SomePara component with sample content

--- SomePara.jsx
+++ SomePara.jsx
@@ -1,1 +1,21 @@
+const SomePara = ()=>{
 
+    return(
+
+        <div>
+
+            <br></br>
+
+            <h1>This is Some Para</h1>
+
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. <br></br>Non, ea veniam! Doloribus voluptas nulla sapiente eligendi! Deserunt obcaecati odio aliquid!</p>
+
+        </div>
+
+    )
+
+}
+
+
+
+export default SomePara
