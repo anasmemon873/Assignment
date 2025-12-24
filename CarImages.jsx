7be7a2b2f1592cea8a89dc8cf008ee6a6150ddff
AnasLalaMemon <c53.rm3760@gmail.com>
2025-12-24 16:19:20 +0500

    Add CarImages component to display car images

--- CarImages.jsx
+++ CarImages.jsx
@@ -1,1 +1,39 @@
+import car1png from './assets/car1.png'
 
+import car2png from './assets/car2.png'
+
+import car3png from './assets/car3.png'
+
+
+
+
+
+const CarImages = ()=>{
+
+    return(
+
+     <div>
+
+        <img src={car1png} alt="car1" />
+
+        <br></br>
+
+        <br></br>
+
+        <img src={car2png} alt="car2" />
+
+        <br></br>
+
+        <br></br>
+
+        <img src={car3png} alt="car3" />
+
+     </div>
+
+    )
+
+}
+
+
+
+export default CarImages
