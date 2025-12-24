f3f9a2880a115e021c15fa21f96bc692609283a7
AnasLalaMemon <c53.rm3760@gmail.com>
2025-12-24 16:17:12 +0500

    Add CarImages and SomePara components to App

--- App.jsx
+++ App.jsx
@@ -1,1 +1,17 @@
+import CarImages from "./CarImages"
+import SomePara from "./SomePara"
 
+
+const App = ()=>{
+  return(
+    <div>
+        <h1>Assignment no.5</h1>
+        <h1>Hello React!</h1>
+        <CarImages/>
+        <SomePara/>
+    </div>
+  )
+}
+
+
+export default App
